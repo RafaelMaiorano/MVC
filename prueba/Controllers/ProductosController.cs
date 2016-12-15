b6925a904972833335cc80fd4a4e157ad1ca94e6
Rafael <maiorano.rafael@hotmail.com>
2016-12-15 17:11:43 -0300

    v1

---
+++ prueba/Controllers/ProductosController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace prueba.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+          //  Models.Mensaje msg = "Use the source Luke!!";
+          string msg = "Use the source Luke!!";
+          //  ViewData.Model = msg;
+          return View(model:msg);
+        }
+    }
+}
